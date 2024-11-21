4a9ea945a456c6ffb688631f94cf08f9c3908ff5
Nisha Kute <157681501+Nishakute@users.noreply.github.com>
2024-11-21 07:59:30 -0800

    Update SigninForm.tsx

--- frontend/src/component/SigninForm.tsx
+++ frontend/src/component/SigninForm.tsx
@@ -15,7 +15,7 @@ const SignIn = () => {
   
     try {
       // Make the POST request to the login endpoint
-      const response = await axios.post("http://localhost:5001/user/login", {
+      const response = await axios.post("https://login-register-otp.vercel.app/?vercelToolbarCode=VNBSyz1IkwgjAlh/user/login", {
         email,
         password,
       });
